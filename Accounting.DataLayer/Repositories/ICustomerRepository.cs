d2292e477063c3ce149d0534295cad33e177aa58
farshad <farshadnp6@gmail.com>
2022-01-30 01:31:05 +0330

    add 2 folder: Repository + Services in DataLayer
    implement CRUD for Customer table. EF and LINQ

---
+++ Accounting.DataLayer/Repositories/ICustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repositories
+{
+    public interface ICustomerRepository
+    {
+        // 5 usual operation SelectAll, Select row, Insert, Update, Delete
+        List<Customers> GetAllCustomers(); //Select all
+        Customers GetCustomersByID(int customerId);  //Select by row
+
+        bool insertCustomer(Customers customer);   // insert
+        bool UpdateCustomer(Customers customer);   // Update
+        
+        bool DeleteCustomer(Customers customer);    //Delete
+        bool DeleteCustomer(int customerId); //Delete by Id
+
+        void save();
+    }
+}
---
+++ Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,89 @@
+﻿using Accounting.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Error!");
+                return false;
+            }
+
+            
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            var customer = GetCustomersByID(customerId);
+            try
+            {
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Error!");
+                return false;
+            }
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomersByID(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+
+        public bool insertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Eror!");
+                return false;
+            }
+        }
+
+        public void save()
+        {
+            db.SaveChanges();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Eror!");
+                return false;
+            }
+        }
+    }
+}
